cc9e3af596777fb82c47bfc6334dc4d3ff638961
DGorgula <gil@dankner.org>
2021-02-26 00:17:06 +0200

    class progress
    finished Card class + created deck class

--- frontend/main.js
+++ frontend/main.js
@@ -1,103 +1,217 @@
-cardDealer = import()
+
+
+
+//  tasks
+    //   1 - create basic deck class with only draw top card method
+    //   2   - create tableDeck: - constructor, shuffle, dealCards and inherit drawTopCard
+    //   3   - pileDeck - method: transition to tableDeck and inherit drawTopCard
+    //   4 - player class diffuclty level its over 9000 !!!!
+    //          -   drawTableCard
+    //          -   drawPileCard
+    //          -   throwCards(1card,2cards,3cards,4cards,seria)
+    //          -   Yaniv
+    //          -   (stickCard)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+// what is this? why all of the page is comment?
+
+
+
 // Use Class name 'Card' with properties 'suit': (Spade, Heart, Club, Diamond), 'rank'(A, 1-10, J, Q, K), 'isJocker'(true, false).
 
 // button 
 
-loadGameSetup(); // players creation
-const players = [{player1}, {player2}, {player3}, {player4}];
-createGame(players) {
-    while(!winner){
-       round(players);
-        playersUpdate(players)
-        //   -optional removePlayers();
-        scoreUpdate(players);
-        showScore();
-    }
+// loadGameSetup(); // players creation
+// const players = [{player1}, {player2}, {player3}, {player4}];
+// createGame(players) {
+//     while(!winner){
+//        round(players);
+//         playersUpdate(players)
+//         //   -optional removePlayers();
+//         scoreUpdate(players);
+//         showScore();
+//     }
 
-}; // Deck => playerDeck TableDeck', 'PileDeck'for each player
-async function round(players){
-const currentPlayer = player[0]
-    while(!Yaniv){
-       await turn(currentPlayer);
-       passTurn();
-    }
+// }; // Deck => playerDeck TableDeck', 'PileDeck'for each player
+// async function round(players){
+// const currentPlayer = player[0]
+//     while(!Yaniv){
+//        await turn(currentPlayer);
+//        passTurn();
+//     }
     
-    scoreCount(players);
+//     scoreCount(players);
 
-};
-startTurn(player);
-callYaniv();
-chooseCard();
+// };
+// startTurn(player);
+// callYaniv();
+// chooseCard();
 
-function removePlayer(player) {
+// function removePlayer(player) {
     
-}
+// }
 
-scoreCount(players) {
-    for (const player in players) {
-        players.score = sum(players.cards);
-    }
-}
+// scoreCount(players) {
+//     for (const player in players) {
+//         players.score = sum(players.cards);
+//     }
+// }
 
-function playersUpdate() {
+// function playersUpdate() {
     
-    if(player.score > 200) {
-      players = removePlayer(playerToRemove);
-    }
-}
-
-YanivDecleration() {
-    
-}
-
+    //     if(player.score > 200) {
+        //       players = removePlayer(playerToRemove);
+        //     }
+        // }
+        
+        // YanivDecleration() {
+            
+            // }
+            // function renderTable(Board);
+            // function renderTable([players], tableDeck, pileDeck);
+            
       // Use Class name 'Deck', and classes 'PlayerDeck', 'TableDeck', and 'PileDeck' which inherit from 'Deck'.
     
      
       
+   
       
-      CLASSES-
-      - Cards
-        - props => (rank, suit, isJoker, value, /*Owner*/ );  // maybe 
-        - methods => getters 
-      - Deck 
-        - props => [Cards];
-        - methods => shuffle, dealCards
-      - Player 
-        - props => playerDeck, name, score, genderAvatar
-        - methods => drawCard, throwCards(1card,2cards,3cards,4cards,seria), Yaniv, (stickCard)
-      OBJECTS-
-        - BOARD 
-        - (Reminders of cards) Deck => ([cards]);
-        - PileDeck => ([cards]);
+    
       
-        function renderTable(Board);
-        function renderTable([players], tableDeck, pileDeck);
       
-      //   Basicly inorder to keep track of all the cards we start by distributing cards from the shuffeld deck to all of the players, than the remaining of the cards are set as the tableDeck. Now we have all of the cards either in the players hand or in the tableDeck. the pileDeck gets its cards from the players and the tableDeck so no double cards. 
-      
+        // - Cards
+        // - props => (rank, suit, isJoker, value); 
+        // - methods => getters    
 class Card {
-  constructor(suit, rank, value, isJoker){
+  constructor(suit, rank, value, isJoker, hidden){
     this.suit = suit;
     this.rank = rank;
     this.value = value;
     this.isJoker = isJoker;
   }
 
-  getCardName() {
+  CardName() {
     return `${this.rank}${this.suit} `;
   }
-  getCardValue() {
+  get CardValue() {
     return this.value ;
   }
-  getIsJoker(){
+  get IsJoker(){
     return this.isJoker;
-  }    
+  } 
+}
+
+// - Deck 
+      //   - props => [Cards];
+      //   - methods => shuffle, dealCards, dealFirstCard
+class Deck {
+  
+  constructor (){
+    this.cards = [];
+    const signs = ['heart','club', 'diamond', 'spade'];
+    const ranksAndValues =[['Ace',1], ['2',2], ['3',3], ['4' ,4], ['5',5], ['6',6], ['7',7], ['8',8], ['9',9], ['10',10], ['Jack',10], ['Queen',10], ['King',10]];
+    for (const sign of signs) {
+      for (const rankAndVal of ranksAndValues) {
+        this.cards.push(new Card(sign,rankAndVal[0],rankAndVal[1],false));
+      }
+
+    }
+    this.cards.push(new Card("Joker","BlackJoker",0,true));
+    this.cards.push(new Card("Joker","RedJoker",0,true));
+  
+  }
+  
+
+  shuffle() {
+    let newCardsArray = [];
+    for(let i = 0 ; i < 54 ; i++) {
+      let index = Math.round(Math.random()*(this.cards.length-1));
+      newCardsArray.push(this.cards[index]);
+      this.cards.splice(index , 1);
+    } 
+    this.cards =  newCardsArray;     
+    }
+
+
+    dealCards() {
+        const fiveCards =  this.cards.splice(0,5);
+        return fiveCards;
+    }
+
+    drawCard() {
+        const topCard =  this.cards.splice(0,1);
+        return topCard;
+    }
 }
 
 
 
+class Player {
+  constructor(playerdeck, name, score, genderAvatar) {
+      this.playerDeck = playerdeck;
+      this.name = name;
+      this.score = score;
+      this.genderAvatar = genderAvatar;
+    }
+    // - Player 
+    //   - props => playerDeck, name, score, genderAvatar
+    //   - methods => drawTableCard, drawPileCard, throwCards(1card,2cards,3cards,4cards,seria), Yaniv, (stickCard)
+    //   - 
+      drawTableCard(deck) {
+        const newCard = deck.drawCard();
+        return newCard;
+      }
+      drawPileCard(pileDeck) {
+        const newCard = pileDeck.drawCard();
+        return newCard;
+      } 
+}
+// const deck = new Deck();
+// console.log(deck);
+// deck.Shuffle();
+// deck.Bla();
+// // console.log(deck[0].CardName());
+// console.log(deck.cards);
+
+let deck = new Deck();
+deck.shuffle();
+player1 = deck.dealCards();
+player2 = deck.dealCards();
+player3 = deck.dealCards();
+player4 = deck.dealCards();
+console.log(deck);
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+  // OBJECTS-
+      //   - BOARD 
+      //   - (Reminders of cards) Deck => ([cards]);
+      //   - PileDeck => ([cards]);
+
+
+      
