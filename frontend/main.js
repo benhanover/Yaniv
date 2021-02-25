03f774b52048ae50815d88335ddfd09a61533cb4
DGorgula <gil@dankner.org>
2021-02-25 16:02:04 +0200

    start planning

---
+++ frontend/main.js
@@ -0,0 +1,3 @@
+createDesk() // Deck, playerDeck for each player
+     // Use Class name 'Card' with properties 'suit': (Spade, Heart, Club, Diamond), 'rank'(A, 1-10, J, Q, K), 'isJocker'(true, false).
+        // Use Class name 'Deck', and classes 'PlayerDeck', 'TableDeck', and 'PileDeck' which inherit from 'Deck'.
