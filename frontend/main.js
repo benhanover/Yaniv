a4bb84425d7ef348aaa98eafd11e533a5d9d4426
DGorgula <gil@dankner.org>
2021-02-25 21:34:07 +0200

    Logic plan completed
    card class created

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,4 @@
-const Dealer = require("card-dealer/dealer");
-const standardDeck = require("card-dealer/decks/standard");
-const dealer = new cardDealer.Dealer(standardDeck);
+cardDealer = import()
 // Use Class name 'Card' with properties 'suit': (Spade, Heart, Club, Diamond), 'rank'(A, 1-10, J, Q, K), 'isJocker'(true, false).
 
 // button 
@@ -54,10 +52,52 @@ YanivDecleration() {
 
       // Use Class name 'Deck', and classes 'PlayerDeck', 'TableDeck', and 'PileDeck' which inherit from 'Deck'.
     
-      // CLASSES-
-      //   - Card: props => suit, rank, isJoker
-      //   - Deck
-      //   - PlayerDeck
-      //   - TableDeck
-      //   - PileDeck
-      //   - Player
+     
+      
+      
+      CLASSES-
+      - Cards
+        - props => (rank, suit, isJoker, value, /*Owner*/ );  // maybe 
+        - methods => getters 
+      - Deck 
+        - props => [Cards];
+        - methods => shuffle, dealCards
+      - Player 
+        - props => playerDeck, name, score, genderAvatar
+        - methods => drawCard, throwCards(1card,2cards,3cards,4cards,seria), Yaniv, (stickCard)
+      OBJECTS-
+        - BOARD 
+        - (Reminders of cards) Deck => ([cards]);
+        - PileDeck => ([cards]);
+      
+        function renderTable(Board);
+        function renderTable([players], tableDeck, pileDeck);
+      
+      //   Basicly inorder to keep track of all the cards we start by distributing cards from the shuffeld deck to all of the players, than the remaining of the cards are set as the tableDeck. Now we have all of the cards either in the players hand or in the tableDeck. the pileDeck gets its cards from the players and the tableDeck so no double cards. 
+      
+class Card {
+  constructor(suit, rank, value, isJoker){
+    this.suit = suit;
+    this.rank = rank;
+    this.value = value;
+    this.isJoker = isJoker;
+  }
+
+  getCardName() {
+    return `${this.rank}${this.suit} `;
+  }
+  getCardValue() {
+    return this.value ;
+  }
+  getIsJoker(){
+    return this.isJoker;
+  }    
+}
+
+
+
+
+
+
+
+
